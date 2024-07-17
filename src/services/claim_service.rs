f5c5b2f91023ca07531320eadc3b941586660a89
phong.bnh <doivjpxx@gmail.com>
2024-07-17 21:39:53 +0700

    feat: add services

---
+++ src/services/claim_service.rs
@@ -0,0 +1,101 @@
+use axum::{
+    async_trait,
+    extract::FromRequestParts,
+    http::{request::Parts, StatusCode},
+    Json, RequestPartsExt,
+};
+use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, TokenData, Validation};
+use once_cell::sync::Lazy;
+
+use axum_extra::{
+    headers::{authorization::Bearer, Authorization},
+    TypedHeader,
+};
+use serde::{Deserialize, Serialize};
+
+use super::user_service::UserResponse;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Claims {
+    pub sub: String,
+    pub username: String,
+    pub iat: usize,
+    pub exp: usize,
+    pub is_sys: Option<bool>,
+}
+
+pub struct Keys {
+    pub encoding: EncodingKey,
+    pub decoding: DecodingKey,
+}
+
+impl Keys {
+    pub fn new(secret: &[u8]) -> Self {
+        Self {
+            encoding: EncodingKey::from_secret(secret),
+            decoding: DecodingKey::from_secret(secret),
+        }
+    }
+}
+
+static KEYS: Lazy<Keys> = Lazy::new(|| {
+    let secret = std::env::var("SECRET_KEY").expect("SECRET_KEY must be set");
+    Keys::new(secret.as_bytes())
+});
+
+impl Claims {
+    pub fn encode_jwt(user: UserResponse) -> Result<String, StatusCode> {
+        let now = chrono::Utc::now();
+        let iat = now.timestamp() as usize;
+        let exp = now.timestamp() as usize + 60 * 60;
+        let claims = Claims {
+            sub: user.email.clone(),
+            username: user.username.clone(),
+            iat,
+            exp,
+            is_sys: None,
+        };
+
+        return encode(&Header::default(), &claims, &KEYS.encoding)
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR);
+    }
+
+    pub fn decode_jwt(jwt: &str) -> Result<TokenData<Claims>, StatusCode> {
+        decode(jwt, &KEYS.decoding, &Validation::default())
+            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+    }
+}
+
+#[async_trait]
+impl<S> FromRequestParts<S> for Claims
+where
+    S: Send + Sync,
+{
+    type Rejection = (StatusCode, Json<serde_json::Value>);
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self, Self::Rejection> {
+        // Extract the token from the authorization header
+        let TypedHeader(Authorization(bearer)) = parts
+            .extract::<TypedHeader<Authorization<Bearer>>>()
+            .await
+            .map_err(|_| {
+                (
+                    StatusCode::UNAUTHORIZED,
+                    Json(serde_json::json!({
+                        "err": "Unauthorized"
+                    })),
+                )
+            })?;
+        // Decode the user data
+        let token_data = Claims::decode_jwt(bearer.token()).map_err(|_| {
+            (
+                StatusCode::UNAUTHORIZED,
+                Json(serde_json::json!({
+                    "err": "Unauthorized"
+                })),
+            )
+        })?;
+
+        Ok(token_data.claims)
+    }
+}
--- src/services/user_service.rs
+++ src/services/user_service.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use super::auth_service::AuthService;
+use super::{auth_service::AuthService, claim_service};
 
 #[derive(Deserialize)]
 pub struct CreateUserRequest {
@@ -10,11 +10,19 @@ pub struct CreateUserRequest {
     pub password: String,
 }
 
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub username: String,
+    pub password: String,
+}
+
 #[derive(Serialize)]
 pub struct UserResponse {
     pub id: uuid::Uuid,
     pub username: String,
+    pub name: String,
     pub email: String,
+    pub is_sys: Option<bool>,
 }
 
 pub struct UserService {
@@ -75,7 +83,44 @@ impl UserService {
         Ok(UserResponse {
             id: user.id,
             username: user.username,
+            name: user.name,
             email: user.email,
+            is_sys: None,
         })
+    }
+
+    pub async fn login(&self, username: String, password: String) -> Result<String, String> {
+        let user = sqlx::query!(
+            r#"
+            SELECT * FROM users WHERE username = $1
+            "#,
+            username
+        )
+        .fetch_one(&self.pool)
+        .await
+        .map_err(|e| {
+            tracing::error!("Failed to get user: {:?}", e);
+            "Failed to get user".to_string()
+        })?;
+
+        let matches = AuthService::verify_password(password, user.password).await;
+
+        if matches.is_err() {
+            return Err("Invalid password".to_string());
+        }
+
+        let jwt = claim_service::Claims::encode_jwt(UserResponse {
+            id: user.id,
+            username: user.username,
+            name: user.name,
+            email: user.email,
+            is_sys: None,
+        })
+        .map_err(|e| {
+            tracing::error!("Failed to encode jwt: {:?}", e);
+            "Failed to encode jwt".to_string()
+        })?;
+
+        Ok(jwt)
     }
 }
