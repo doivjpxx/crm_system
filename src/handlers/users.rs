60375dd5858fd932f9d27b88050061801c65364e
phong.bnh <doivjpxx@gmail.com>
2024-07-17 21:40:03 +0700

    update models

--- src/handlers/users.rs
+++ src/handlers/users.rs
@@ -1,10 +1,18 @@
 use std::sync::Arc;
 
-use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum::{
+    extract::{Path, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
 
 use crate::{
     app::AppState,
-    services::user_service::{CreateUserRequest, UserService},
+    services::{
+        claim_service::Claims,
+        user_service::{CreateUserRequest, LoginRequest, UserService},
+    },
 };
 
 pub async fn create_user(
@@ -18,6 +26,37 @@ pub async fn create_user(
             StatusCode::CREATED,
             Json(serde_json::json!({ "message": "User created" })),
         )),
+        Err(e) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({ "error": e })),
+        )),
+    }
+}
+
+pub async fn get_user(
+    claims: Claims,
+    Path(username): Path<String>,
+    State(state): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let user_service = UserService::new(state.pool.clone());
+
+    match user_service.get_user(username).await {
+        Ok(user) => Ok((StatusCode::OK, Json(user))),
+        Err(e) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Json(serde_json::json!({ "error": e })),
+        )),
+    }
+}
+
+pub async fn login(
+    State(state): State<Arc<AppState>>,
+    Json(user): Json<LoginRequest>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let user_service = UserService::new(state.pool.clone());
+
+    match user_service.login(user.username, user.password).await {
+        Ok(token) => Ok((StatusCode::OK, Json(serde_json::json!({ "token": token })))),
         Err(e) => Err((
             StatusCode::INTERNAL_SERVER_ERROR,
             Json(serde_json::json!({ "error": e })),
--- src/router.rs
+++ src/router.rs
@@ -7,12 +7,17 @@ use axum::{
 
 use crate::{
     app::AppState,
-    handlers::{health::health, users::create_user},
+    handlers::{
+        health::health,
+        users::{create_user, get_user, login},
+    },
 };
 
 pub fn create_router(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/health", get(health))
         .route("/user", post(create_user))
+        .route("/login", post(login))
+        .route("/user/:id", get(get_user))
         .with_state(app_state)
 }
